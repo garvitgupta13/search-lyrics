2e0fd2d712143afaeb6f29539994670ecae67fae
garvitgupta13 <garvitgupta2001@gmail.com>
2021-04-14 01:39:25 +0530

    Replaced browserRouter with hashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import Footer from "./components/footer";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
     <Footer />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 reportWebVitals();
